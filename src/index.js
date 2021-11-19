204b37528607735059af90569d963ebdcd30e935
ChornaEva <chornaja.evgenija@gmail.com>
2021-11-19 12:36:28 +0200

    load more button test done

--- src/index.js
+++ src/index.js
@@ -1,34 +1,29 @@
 import './css/styles.css';
-import debounce from 'lodash.debounce';
 import Notiflix from 'notiflix';
 import axios from 'axios';
 
 axios.defaults.baseURL = 'https://pixabay.com/api';
+
 const searchForm = document.querySelector('#search-form');
 const loadMoreButton = document.querySelector('.load-more');
 const galleryList = document.querySelector('.gallery');
+
 let searchImage = "";
-let imageCardData = [];
+let page = 1;
+let imageLimit = 3;
 
-searchForm.addEventListener('submit', event => {
-    event.preventDefault();
-    searchImage = searchForm.searchQuery.value.trim();
-    getImages(searchImage).then(createCard);
-});
 
+searchForm.addEventListener('submit', onSubmit);
 loadMoreButton.addEventListener('click', onLoadMore);
 
-
-
- function getImages(searchImage) {
-    return axios.get(`/?key=24369719-4937f00e9b76df3c43c2e5aa7&q=${searchImage}&image_type=photo&orientation=horizontal&safesearch=true`);
+function getImages(searchImage) {
+    return axios.get(`/?key=24369719-4937f00e9b76df3c43c2e5aa7&q=${searchImage}&image_type=photo&orientation=horizontal&safesearch=true&per_page=${imageLimit}&page=${page}`);
 };
 
 
-function createCard(image) {
+function onSubmitcreateCard(image) {
 
-    imageCardData = image.data.hits;
-    // console.log(imageCardData.length);
+   const imageCardData = image.data.hits;
 
     if (imageCardData.length === 0) {
        Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.'); 
@@ -57,10 +52,44 @@ function createCard(image) {
     }
 };
 
-function onLoadMore(searchImage) {
- getImages(searchImage).then(createCard);    
+function onLoadMoreCreateCard(image) {
+    const imageCardData = image.data.hits;
+    
+    imageCardData.map(({ webformatURL, tags, likes, views, comments, downloads }) => {
+       const string = `<div class="photo-card">
+                <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+                <div class="info">
+                    <p class="info-item">
+                    <b>Likes: ${likes}</b>
+                    </p>
+                    <p class="info-item">
+                    <b>Views: ${views}</b>
+                    </p>
+                    <p class="info-item">
+                    <b>Comments: ${comments}</b>
+                    </p>
+                    <p class="info-item">
+                    <b>Downloads: ${downloads}</b>
+                    </p>
+                </div>
+                </div>`;
+        galleryList.insertAdjacentHTML('beforeend', string);
+    })
+ }
+
+function onLoadMore() {
+    page += 1;
+    getImages(searchImage).then(onLoadMoreCreateCard);
+    
+console.log(page);
 };
 
+function onSubmit() {
+    event.preventDefault();
+    page = 1;
+    searchImage = searchForm.searchQuery.value.trim();
+    getImages(searchImage).then(onSubmitcreateCard); 
+}
 
 
 
@@ -75,20 +104,11 @@ function onLoadMore(searchImage) {
 
 
 
+// Первый вариант слушателя на форме:
 
+// searchForm.addEventListener('submit', event => {
+//     // event.preventDefault();
 
-
-
-// function showGalleryProfile() {
-//   showImages(gallery);
-// }
-    
-// function inputValidation(string) {
-//     if (searchForm.searchQuery.value === "") {
-//         galleryList.innerHTML = "";
-//     }
-//     else {
-        
-//         fetchCountries(string).then(showGalleryProfile).catch(showError).finally(()=>searchForm.searchQuery.value === "");
-//     }
-// }
+//     searchImage = searchForm.searchQuery.value.trim();
+//     getImages(searchImage).then(onSubmitcreateCard);
+// });
